e5047ceae29f4cf5bc331e182ed615962582d4aa
Ken Courville <krcourville@gmail.com>
2015-09-16 15:21:05 -0600

    initial source

---
+++ TCApp.Console/Actions.cs
@@ -0,0 +1,28 @@
+﻿using CLAP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCApp.WebServices.SubmitResumeWebService;
+
+namespace TCApp.Cmd
+{
+    class Actions
+    {
+        SubmitResumeSoapClient client = new SubmitResumeSoapClient();
+
+        [Verb]
+        void GetInstructions()
+        {
+            Console.WriteLine("INSTRUCTIONS.. SHOULD YOU CHOOSE TO ACCEPT:\n\n{0}", client.GetInstructions());
+        }
+
+        [Verb]
+        void SubmitInformation([Required]string fname, [Required]string lname, [Required]string email, [Required]string phone)
+        {
+            var furtherInstructions = client.SubmitInformation(fname, lname, email, phone);
+            Console.WriteLine("FURTHER INSTRUCTIONS... SHOULD YOU CHOOSE TO ACCEPT:\n\n{0}", furtherInstructions);
+        }
+    }
+}
---
+++ TCApp.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using CLAP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCApp.Cmd
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args == null || !args.Any())
+            {
+                args = new[] { "h" };
+            }
+
+            var actions = new Actions();
+            return Parser.RunConsole(args, actions);
+        }
+    }
+}
